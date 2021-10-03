c01f653178f77040e151fc0746e3b1c6834a196e
Jin Park <jinpark.dev@gmail.com>
2021-10-03 22:08:46 +0900

    Physics based character

---
+++ Assets/Scripts/ABController.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace MetaBoxing
+{
+    public class ABController : MonoBehaviour
+    {
+        public Transform kinematicBody;
+
+        private Quaternion _defaultRot;
+        private Quaternion _inverseDefaultRot;
+        private ArticulationBody _ab;
+
+        private void Start()
+        {
+            Assert.IsNotNull(kinematicBody, "Set the target");
+            _defaultRot = transform.rotation;
+            _inverseDefaultRot = Quaternion.Inverse(_defaultRot);
+            _ab = GetComponent<ArticulationBody>();
+            Assert.IsNotNull(_ab, "You need a ArticulationBody component");
+        }
+
+        private void FixedUpdate()
+        {
+            var targetAngle = (_inverseDefaultRot * kinematicBody.rotation).eulerAngles;
+            targetAngle.x = Mathf.DeltaAngle(0, targetAngle.x);
+            targetAngle.y = Mathf.DeltaAngle(0, targetAngle.y);
+            targetAngle.z = Mathf.DeltaAngle(0, targetAngle.z);
+
+
+            if (_ab.linearLockX != ArticulationDofLock.LockedMotion)
+            {
+                var drive = _ab.xDrive;
+                drive.target = Mathf.Clamp(targetAngle.x, drive.lowerLimit, drive.upperLimit);
+                _ab.xDrive = drive;
+            }
+
+            if (_ab.linearLockY != ArticulationDofLock.LockedMotion)
+            {
+                var drive = _ab.yDrive;
+                drive.target = Mathf.Clamp(targetAngle.y, drive.lowerLimit, drive.upperLimit);
+                _ab.yDrive = drive;
+            }
+
+            if (_ab.linearLockZ != ArticulationDofLock.LockedMotion)
+            {
+                var drive = _ab.zDrive;
+                drive.target = Mathf.Clamp(targetAngle.z, drive.lowerLimit, drive.upperLimit);
+                _ab.zDrive = drive;
+            }
+        }
+    }
+}
