36b80c41910c38395c674ae74f991b5f892e03c6
Jin Park <jinpark.dev@gmail.com>
2021-10-17 20:01:39 +0900

    Better physics

--- Assets/Scripts/ABController.cs
+++ Assets/Scripts/ABController.cs
@@ -7,36 +7,39 @@ namespace MetaBoxing
     {
         public Transform kinematicBody;
 
-        private Quaternion _defaultRot;
-        private Quaternion _inverseDefaultRot;
         private ArticulationBody _ab;
 
         private void Start()
         {
             Assert.IsNotNull(kinematicBody, "Set the target");
-            _defaultRot = transform.localRotation;
-            _inverseDefaultRot = Quaternion.Inverse(_defaultRot);
             _ab = GetComponent<ArticulationBody>();
             Assert.IsNotNull(_ab, "You need a ArticulationBody component");
         }
 
         private void FixedUpdate()
         {
-            var targetAngle = (_inverseDefaultRot * kinematicBody.localRotation).eulerAngles;
-
-            if (_ab.name.Equals("LowerArm_L"))
+            if (_ab.name.Equals("LowerArm_L") || _ab.name.Equals("LowerArm_R"))
             {
-                targetAngle = kinematicBody.localRotation.eulerAngles; // TODO: Not matching
-                
-                print($"kine:{kinematicBody.localRotation.eulerAngles} target:{targetAngle}");
+                var elbowPos = kinematicBody.position;
+                var rootPos = kinematicBody.parent.position;
+                var handPos = kinematicBody.GetChild(0).position;
+
+                var angle = Vector3.Angle(rootPos - elbowPos, handPos - elbowPos);
+                var drive = _ab.xDrive;
+                drive.target = 180f - angle;
+                _ab.xDrive = drive;
+                return;
             }
+
+            var targetAngle = (Quaternion.Inverse(_ab.parentAnchorRotation) *
+                               kinematicBody.localRotation * _ab.anchorRotation).eulerAngles;
 
             if (_ab.linearLockX != ArticulationDofLock.LockedMotion)
             {
                 targetAngle.x = Mathf.DeltaAngle(0, targetAngle.x);
                 var drive = _ab.xDrive;
                 // if (!_ab.name.Equals("LowerArm_L"))
-                //     targetAngle.x = Mathf.Clamp(targetAngle.x, drive.lowerLimit, drive.upperLimit);
+                targetAngle.x = Mathf.Clamp(targetAngle.x, drive.lowerLimit, drive.upperLimit);
                 drive.target = targetAngle.x;
                 _ab.xDrive = drive;
             }
@@ -46,7 +49,7 @@ namespace MetaBoxing
                 targetAngle.y = Mathf.DeltaAngle(0, targetAngle.y);
                 var drive = _ab.yDrive;
                 // if (!_ab.name.Equals("LowerArm_L"))
-                //     targetAngle.y = Mathf.Clamp(targetAngle.y, drive.lowerLimit, drive.upperLimit);
+                targetAngle.y = Mathf.Clamp(targetAngle.y, drive.lowerLimit, drive.upperLimit);
                 drive.target = targetAngle.y;
                 _ab.yDrive = drive;
             }
@@ -56,7 +59,7 @@ namespace MetaBoxing
                 targetAngle.z = Mathf.DeltaAngle(0, targetAngle.z);
                 var drive = _ab.zDrive;
                 // if (!_ab.name.Equals("LowerArm_L"))
-                //     targetAngle.z = Mathf.Clamp(targetAngle.z, drive.lowerLimit, drive.upperLimit);
+                targetAngle.z = Mathf.Clamp(targetAngle.z, drive.lowerLimit, drive.upperLimit);
                 drive.target = targetAngle.z;
                 _ab.zDrive = drive;
             }
