7a704e8be8fff9265a75cbc81c2f6adffd239aa3
Jin Park <jinpark.dev@gmail.com>
2021-11-09 17:22:53 +0900

    Reward

---
+++ Assets/Scripts/AgentCollisionDetector.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+namespace MetaBoxing
+{
+    public class AgentCollisionDetector : MonoBehaviour
+    {
+        private ArticulationBody _body;
+        private BoxingAgent _myself;
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+            _body = GetComponent<ArticulationBody>();
+            _myself = GetComponentInParent<BoxingAgent>();
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            if (other.gameObject.layer != LayerMask.NameToLayer("Agent")) return;
+
+            if (other.gameObject.GetComponentInParent<BoxingAgent>() == _myself) return;
+
+            var ab = other.gameObject.GetComponent<ArticulationBody>();
+            var score = (ab.velocity - _body.velocity).magnitude;
+
+            _myself.opponent.myScore += score;
+            _myself.myNegScore += score;
+
+
+            var xDrive = _body.xDrive;
+            xDrive.stiffness = 0;
+            _body.xDrive = xDrive;
+
+            var zDrive = _body.zDrive;
+            zDrive.stiffness = 0;
+            _body.zDrive = zDrive;
+        }
+    }
+}
--- Assets/Scripts/BoxingAgent.cs
+++ Assets/Scripts/BoxingAgent.cs
@@ -1,66 +1,121 @@
-using System.Collections.Generic;
+using TMPro;
 using Unity.MLAgents;
 using Unity.MLAgents.Sensors;
 using UnityEngine;
 
-public class BoxingAgent : Agent
+namespace MetaBoxing
 {
-    public ArticulationBody hipsAB;
-    public ArticulationBody upperArmLAB;
-    public ArticulationBody lowerArmLAB;
-    public ArticulationBody upperArmRAB;
-    public ArticulationBody lowerArmRAB;
-
-    public SphereCollider head;
-
-    public override void Initialize()
+    public class BoxingAgent : Agent
     {
-    }
+        public ArticulationBody hipsAB;
+        public ArticulationBody upperArmLAB;
+        public ArticulationBody lowerArmLAB;
+        public ArticulationBody upperArmRAB;
+        public ArticulationBody lowerArmRAB;
 
-    public override void OnEpisodeBegin()
-    {
-    }
+        private AgentCollisionDetector _cd;
 
-    public override void CollectObservations(VectorSensor sensor)
-    {
-        sensor.AddObservation(transform.position);
-    }
+        public BoxingAgent opponent;
 
-    public override void OnActionReceived(float[] vectorAction)
-    {
-        var index = 0;
+        public float myScore = 0f;
+        private float _myCumScore = 0f;
+        public float myNegScore = 0f;
+        private float _myCumNegScore = 0f;
 
-        hipsAB.xDrive = SetDriveTarget(hipsAB.xDrive, vectorAction[index++]);
-        hipsAB.zDrive = SetDriveTarget(hipsAB.zDrive, vectorAction[index++]);
-        
-        upperArmLAB.xDrive = SetDriveTarget(upperArmLAB.xDrive, vectorAction[index++]);
-        upperArmLAB.yDrive = SetDriveTarget(upperArmLAB.yDrive, vectorAction[index++]);
-        upperArmLAB.zDrive = SetDriveTarget(upperArmLAB.zDrive, vectorAction[index++]);
-        lowerArmLAB.xDrive = SetDriveTarget(lowerArmLAB.xDrive, vectorAction[index++]);
-        
-        upperArmRAB.xDrive = SetDriveTarget(upperArmRAB.xDrive, vectorAction[index++]);
-        upperArmRAB.yDrive = SetDriveTarget(upperArmRAB.yDrive, vectorAction[index++]);
-        upperArmRAB.zDrive = SetDriveTarget(upperArmRAB.zDrive, vectorAction[index++]);
-        lowerArmRAB.xDrive = SetDriveTarget(lowerArmRAB.xDrive, vectorAction[index++]);
-        
-        if (index != vectorAction.Length) Debug.LogError($"{index} is smaller than {vectorAction.Length}");
-    }
+        public TextMeshPro tmp;
+        public int steps = 0;
 
-    public override void Heuristic(float[] actionsOut)
-    {
-        for (var i = 0; i < actionsOut.Length; i++) actionsOut[i] = Random.Range(-1f, 1f);
-    }
+        public override void Initialize()
+        {
+            _cd = GetComponentInChildren<AgentCollisionDetector>();
+            if (opponent != default) opponent.opponent = this;
+        }
 
-    private ArticulationDrive SetDriveTarget(ArticulationDrive drive, float x)
-    {
-        var range = drive.upperLimit - drive.lowerLimit;
-        var target = drive.lowerLimit + range / 2 * (x + 1);
-        drive.target = target;
-        return drive;
-    }
+        public override void OnEpisodeBegin()
+        {
+            myScore = 0f;
+            _myCumScore = 0f;
+            myNegScore = 0f;
+            _myCumNegScore = 0f;
+            tmp.text = $"{(int) _myCumScore}";
 
-    private void FixedUpdate()
-    {
-        RequestDecision();
+            steps = 0;
+        }
+
+        public override void CollectObservations(VectorSensor sensor)
+        {
+            sensor.AddObservation(transform.position);
+        }
+
+        public override void OnActionReceived(float[] vectorAction)
+        {
+            var index = 0;
+
+            hipsAB.xDrive = SetDriveTarget(hipsAB.xDrive, vectorAction[index++]);
+            hipsAB.zDrive = SetDriveTarget(hipsAB.zDrive, vectorAction[index++]);
+
+            upperArmLAB.xDrive = SetDriveTarget(upperArmLAB.xDrive, vectorAction[index++]);
+            upperArmLAB.yDrive = SetDriveTarget(upperArmLAB.yDrive, vectorAction[index++]);
+            upperArmLAB.zDrive = SetDriveTarget(upperArmLAB.zDrive, vectorAction[index++]);
+            lowerArmLAB.xDrive = SetDriveTarget(lowerArmLAB.xDrive, vectorAction[index++]);
+
+            upperArmRAB.xDrive = SetDriveTarget(upperArmRAB.xDrive, vectorAction[index++]);
+            upperArmRAB.yDrive = SetDriveTarget(upperArmRAB.yDrive, vectorAction[index++]);
+            upperArmRAB.zDrive = SetDriveTarget(upperArmRAB.zDrive, vectorAction[index++]);
+            lowerArmRAB.xDrive = SetDriveTarget(lowerArmRAB.xDrive, vectorAction[index++]);
+
+            if (index != vectorAction.Length) Debug.LogError($"{index} is smaller than {vectorAction.Length}");
+        }
+
+        public override void Heuristic(float[] actionsOut)
+        {
+            for (var i = 0; i < actionsOut.Length; i++) actionsOut[i] = Random.Range(-1f, 1f);
+        }
+
+        private ArticulationDrive SetDriveTarget(ArticulationDrive drive, float x)
+        {
+            var range = drive.upperLimit - drive.lowerLimit;
+            var target = drive.lowerLimit + range / 2 * (x + 1);
+            drive.target = target;
+            return drive;
+        }
+
+        private void FixedUpdate()
+        {
+            // TODO:
+            if (myScore > 0)
+            {
+                AddReward(myScore / 10f);
+                _myCumScore += myScore;
+                myScore = 0;
+                tmp.text = $"{(int) _myCumScore}";
+            }
+
+            if (myNegScore > 0)
+            {
+                AddReward(myNegScore / -20f);
+                _myCumNegScore += myNegScore;
+                myNegScore = 0;
+            }
+
+            if (_myCumScore > 50)
+            {
+                AddReward(1f);
+                print($"{GetCumulativeReward()} and win");
+                EndEpisode();
+            }
+            else if (_myCumNegScore > 50)
+            {
+                AddReward(-1f);
+                print($"{GetCumulativeReward()} and lose");
+                EndEpisode();
+            }
+
+            ++steps;
+            AddReward(-0.001f);
+            if (steps > 2000)
+                EndEpisode();
+            RequestDecision();
+        }
     }
 }
