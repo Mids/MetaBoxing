65f5b7adef4c081df2776e012d77b2e44b4bb8bc
Jin Park <jinpark.dev@gmail.com>
2021-12-11 20:16:27 +0900

    Physics 1PP bug fix

--- Assets/Scripts/VCamFollower.cs
+++ Assets/Scripts/VCamFollower.cs
@@ -1,11 +1,12 @@
 using Cinemachine;
+using Photon.Pun;
 using UnityEngine;
 
 public class VCamFollower : MonoBehaviour
 {
     private void Start()
     {
-        if (PlayerPrefs.GetInt("ISPHYSICS") == 0 || PlayerPrefs.GetInt("IS3PP") == 1)
+        if (PlayerPrefs.GetInt("ISPHYSICS") == 0 || PlayerPrefs.GetInt("IS3PP") == 1 || !GetComponentInParent<PhotonView>().IsMine)
             GetComponent<CinemachineVirtualCamera>().enabled = false;
     }
 }
