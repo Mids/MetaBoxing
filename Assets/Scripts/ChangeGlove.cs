56ee8def23de7639099bc48badbafc526136a9d9
Jin Park <jinpark.dev@gmail.com>
2021-12-14 16:13:34 +0900

    Opponent glove color

--- Assets/Scripts/ChangeGlove.cs
+++ Assets/Scripts/ChangeGlove.cs
@@ -6,7 +6,7 @@ public class ChangeGlove : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        if (GetComponentInParent<PhotonView>().IsMine)
+        if (!GetComponentInParent<PhotonView>().IsMine)
         {
             var blueMat = Resources.Load<Material>("Meshes/Characters/Gloves_MAT_Blue");
             GetComponent<SkinnedMeshRenderer>().material = blueMat;
